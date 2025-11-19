3a45fbd4312524ffc9b3b3403d1a251e48333617
aikwen <kwen.zou@outlook.com>
2025-11-20 00:56:45 +0800

    :sparkle: add logger test

---
+++ tests/test_log.py
@@ -0,0 +1,10 @@
+from owl.utils import file_io
+
+if __name__ == '__main__':
+
+    f1 = file_io.create_logger("2025-11-19.log")
+    f2 = file_io.create_logger("2025-11-19.log")
+
+    f1.info("abc1")
+    f2.info("abc2")
+
