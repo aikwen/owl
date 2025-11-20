a9b70bdd8cf2fec1e6ddb579931060e837187998
aikwen <kwen.zou@outlook.com>
2025-11-20 15:43:00 +0800

    :sparkles: add logging

--- src/owl/core/engine.py
+++ src/owl/core/engine.py
@@ -111,30 +111,32 @@ class OwlEngine:
         构造整个 engine
         :return:
         """
+        logger = file_io.create_logger(self.log_name, "train")
         # 检查模型
         if self.status.model is None:
             raise RuntimeError("❌ Engine Build Error: Model 未配置。")
-
+        logger.info("✅ 初始化模型")
         # 检查数据集
         if self.status.train_loader is None:
             raise RuntimeError("❌ Engine Build Error: TrainLoader 未配置。")
-
-
+        logger.info("✅ 初始化训练数据集")
         if self.epochs <= 0:
             raise RuntimeError("❌ Engine Build Error: epochs 必须大于等于 0")
-
+        logger.info("✅ 初始化epoch")
         # 检查优化器
         if self.status.optimizer is None:
             raise RuntimeError("❌ Engine Build Error: Optimizer 未配置。")
-
+        logger.info("✅ 初始化优化器")
         # 检查学习率优化器
         if self.status.scheduler is None:
             # 可以没有学习率优化器，暂时先 pass， 后续写到日志里面
-            pass
-
+            logger.warning("❌ 学习率优化器未设置")
+        else:
+            logger.info("✅ 初始化学习率优化器")
         # 检查损失函数
         if self.criterion is None:
             raise RuntimeError("❌ Engine Build Error: criterion 未配置。")
+        logger.info("✅ 初始化损失函数")
 
         # 当训练模式是断点续训或微调的时候，检查是否提供权重文件
         if self.train_mode != TrainMode.TRAIN and self.pre_checkpoint is None:
@@ -147,19 +149,22 @@ class OwlEngine:
         # 设置 device
         self.status.model.to(self.status.device)
         self.criterion.to(self.status.device)
-
+        logger.info(f"✅ 当前训练设备：{self.status.device}")
         # 断点续训
         if self.train_mode == TrainMode.RESUME:
             self.status.load_state_dict(self.pre_checkpoint, only_model=False)
         # 迁移学习
         elif self.train_mode == TrainMode.FINETUNE:
             self.status.load_state_dict(self.pre_checkpoint, only_model=True)
-
+        logger.info(f"✅ 当前训练模式：{self.train_mode}")
         # 创建权重输出文件夹
-        file_io.create_dir(self.checkpoint_dir)
-
+        if self.autosave:
+            file_io.create_dir(self.checkpoint_dir)
+            logger.info(f"✅ 权重保存目录：{self.checkpoint_dir}")
+        logger.info(f"{'✅' if self.autosave else '❌'} 权重自动保存：{self.autosave}")
         # build 结束
         self._is_built = True
+        logger.info("✅ 构建结束!")
         return self
 
     def save_checkpoint(self):
@@ -169,9 +174,11 @@ class OwlEngine:
 
 
     def train_one_epoch(self):
+        logger = file_io.create_logger(self.log_name, "train")
         self.status.model.train()
         loader = self.status.train_loader
 
+        interval_loss = 0.0
         for i, batch in enumerate(loader):
             tp, gt, _, _ = batch
             tp = tp.to(self.status.device)
@@ -187,10 +194,18 @@ class OwlEngine:
             if self.status.scheduler is not None:
                 self.status.scheduler.step()
 
+            interval_loss += loss.item()
+            # 每 10 轮打印平均损失和当前的学习率
+            if i % 10 == 0:
+                interval_loss = f"{interval_loss / 10:.5f}"
+                cur_lr = f"{self.status.optimizer.param_groups[0]['lr']:.8f}"
+                logger.info(f"epoch-{self.status.epoch:03d} | {i-10}~{i} batch avg loss {interval_loss} | lr:{cur_lr}")
+
     def run(self):
         if not self._is_built:
             self.build()
-
+        logger = file_io.create_logger(self.log_name, "train")
+        logger.info("✅ 开始训练")
         # 训练
         for epoch in range(self.status.epoch, self.epochs):
             self.status.epoch = epoch
@@ -199,5 +214,5 @@ class OwlEngine:
             if self.autosave:
                 self.save_checkpoint()
 
-        # 暂时不验证
+            # 暂时不验证
 
