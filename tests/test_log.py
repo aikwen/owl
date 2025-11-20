30bcb1ed6ab39087cfa6acfbc81aa6fcba45cb13
aikwen <kwen.zou@outlook.com>
2025-11-20 15:42:35 +0800

    :white_check_mark: test logger

--- tests/test_log.py
+++ tests/test_log.py
@@ -2,9 +2,10 @@ from owl.utils import file_io
 
 if __name__ == '__main__':
 
-    f1 = file_io.create_logger("2025-11-19.log")
-    f2 = file_io.create_logger("2025-11-19.log")
+    f1 = file_io.create_logger("2025-11-19","train")
+    f2 = file_io.create_logger("2025-11-19", "train")
 
     f1.info("abc1")
     f2.info("abc2")
+    f1.warning("abc3")
 
