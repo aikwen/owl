6d38e3cf755d16a1915df473da8da7cd7e90100a
aikwen <kwen.zou@outlook.com>
2025-11-28 17:18:22 +0800

    ♻️ refactor engine

--- src/owl/core/engine.py
+++ src/owl/core/engine.py
@@ -5,7 +5,8 @@ from pathlib import Path
 
 from typing import  Optional, Any, Dict
 from ..utils import types, file_io, console
-from ..utils.types import TrainMode
+from ..utils.types import TrainMode, OwlFactory
+from ..utils import validator
 from .status import Status
 
 _welcome_is_print:bool = False
@@ -18,7 +19,7 @@ class OwlEngine:
         self.epochs: int = 0
         # 损失函数
         self.criterion: Optional[nn.Module] = None
-        # 模式
+        # 训练模式
         self.train_mode:types.TrainMode = TrainMode.TRAIN
         self.pre_checkpoint:Optional[Dict[str, Any]] = None
         # 输出权重文件夹名和日志名
@@ -30,24 +31,8 @@ class OwlEngine:
         self._is_built:bool = False
         # 验证函数
         self._is_val: bool = False
-
-    def config_model(self, model:nn.Module) -> 'OwlEngine':
-        """
-        配置模型
-        :param model:
-        :return:
-        """
-        self.status.model = model
-        return self
-
-    def config_dataloader(self, train_loader: DataLoader) -> 'OwlEngine':
-        """
-        配置训练集数据加载器
-        :param train_loader: DataLoader
-        :return:
-        """
-        self.status.train_loader = train_loader
-        return self
+        # 工厂类
+        self.factory: Optional[OwlFactory] = None
 
     def config_val(self, val_loader: Dict[str, DataLoader]) -> 'OwlEngine':
         """
@@ -59,27 +44,23 @@ class OwlEngine:
         self.status.val_loader = val_loader
         return self
 
-    def config_epochs(self, epochs:int) -> 'OwlEngine':
-        self.epochs = epochs
-        return self
 
-    def config_optimizer(self, optimizer:torch.optim.Optimizer) -> 'OwlEngine':
+    def config_factory(self, factory: OwlFactory) -> 'OwlEngine':
         """
-        配置优化器
-        :param optimizer:
+        工厂
+        :param factory:
         :return:
         """
-        self.status.optimizer = optimizer
+        self.factory = factory
         return self
 
-    def config_scheduler(self, scheduler: Any) -> 'OwlEngine':
-        """配置学习率调度器"""
-        self.status.scheduler = scheduler
-        return self
-
-    def config_loss(self, criterion: nn.Module) -> 'OwlEngine':
-        """配置损失函数"""
-        self.criterion = criterion
+    def config_epochs(self, epochs:int) -> 'OwlEngine':
+        """
+        配置 epoch 数量
+        :param epochs:
+        :return:
+        """
+        self.epochs = epochs
         return self
 
     def config_device(self, device:torch.device) -> 'OwlEngine':
@@ -124,55 +105,57 @@ class OwlEngine:
         if not _welcome_is_print:
             console.welcome()
             _welcome_is_print = True
-        # 检查是否设置了日志输出文件
-        if self.log_name == "":
-            raise RuntimeError("❌ Engine Build Error: 日志文件名未配置。")
-        logger = file_io.create_logger(self.log_name, "train")
 
-        # 检查模型
-        if self.status.model is None:
-            raise RuntimeError("❌ Engine Build Error: Model 未配置。")
+        # 检查是否设置了日志输出文件
+        validator.check(self.log_name != "", "❌ Engine Build Error: 日志文件名未配置。")
+        logger = file_io.create_logger(self.log_name, "train")
+        # 打印当前设备
+        logger.info(f"✅ 当前训练设备：{self.status.device}")
+        # ========= 组装模型 ========
+        validator.check(self.factory is not None, "❌ Engine Build Error: 未配置 OwlFactory")
+        # 创建模型
+        self.status.model = self.factory.create_model()
+        validator.check(self.status.model is not None, "❌ Engine Build Error: Model 未正确初始化。")
         logger.info("✅ 初始化模型")
 
-        # 检查训练数据集
-        if self.status.train_loader is None:
-            raise RuntimeError("❌ Engine Build Error: TrainLoader 未配置。")
-        logger.info(f"✅ 初始化训练数据集, 数量: {len(self.status.train_loader)}")
-
-        # 检查验证数据集
-        if self.status.val_loader is None or len(self.status.val_loader) == 0:
-            logger.warning("⚠️ 未添加验证数据集")
-        else:
-            logger.info(f"✅ 初始化验证数据集: {[name for name in self.status.val_loader.keys()]}")
-
-        # 检查 epoch
-        if self.epochs <= 0:
-            raise RuntimeError("❌ Engine Build Error: epochs 必须大于等于 0")
-        logger.info(f"✅ 初始化epoch: {self.epochs}")
-
-        # 检查优化器
-        if self.status.optimizer is None:
-            raise RuntimeError("❌ Engine Build Error: Optimizer 未配置。")
-        logger.info(f"✅ 初始化优化器,初始化学习率: {self.status.optimizer.param_groups[0]['lr']:.8f}")
-
-        # 检查学习率优化器
-        if self.status.scheduler is None:
-            # 可以没有学习率优化器，暂时先 pass， 后续写到日志里面
-            logger.warning("⚠️ 学习率优化器未设置")
-        else:
-            logger.info(f"✅ 初始化学习率优化器")
-
-        # 检查损失函数
-        if self.criterion is None:
-            raise RuntimeError("❌ Engine Build Error: criterion 未配置。")
+        # 创建损失函数
+        self.criterion = self.factory.create_criterion()
+        validator.check(self.criterion is not None, "❌ Engine Build Error: criterion 未正确初始化。")
         logger.info("✅ 初始化损失函数")
 
-        # 当训练模式是断点续训或微调的时候，检查是否提供权重文件
+        # ⚠️⚠️⚠️ 模型和损失函数都移动到 device 中
+        self.status.model.to(self.status.device)
+        self.criterion.to(self.status.device)
+
+        # 检查训练数据集
+        self.status.train_loader = self.factory.create_train_dataloader()
+        validator.check(self.status.train_loader is not None, "❌ Engine Build Error: TrainLoader 未配置。")
+        logger.info(f"✅ 初始化训练数据集, batch_size: {self.status.train_loader.batch_size}")
+
+        # 检查 epoch
+        validator.check(self.epochs > 0, "❌ Engine Build Error: epochs 必须大于等于 0")
+        logger.info(f"✅ 初始化epoch: {self.epochs}")
+
+        # 创建优化器
+        self.status.optimizer = self.factory.create_optimizer(self.status.model)
+        validator.check(self.status.optimizer is not None, "❌ Engine Build Error: Optimizer 未正确初始化。")
+        logger.info(f"✅ 初始化优化器")
+
+        # 创建学习率调整器
+        self.status.scheduler = self.factory.create_scheduler(optimizer=self.status.optimizer,
+                                                              epochs=self.epochs,
+                                                              batches=len(self.status.train_loader))
+        if self.status.scheduler is None:
+            logger.warning("⚠️ 学习率调整器未设置")
+        else:
+            logger.info(f"✅ 初始化学习率调整器")
+
+        # 检查训练模式：当训练模式是断点续训或微调的时候，检查是否提供权重文件
+        logger.info(f"✅ 当前训练模式：{self.train_mode}")
         if self.train_mode != TrainMode.TRAIN and self.pre_checkpoint is None:
             raise RuntimeError("❌ Engine Build Error: pre_checkpoint 未配置。")
 
-
-        logger.info(f"✅ 当前训练模式：{self.train_mode}")
+        # 加载预训练或者断点续训权重
         # 断点续训
         if self.train_mode == TrainMode.RESUME:
             self.status.load_state_dict(self.pre_checkpoint, only_model=False)
@@ -182,15 +165,18 @@ class OwlEngine:
             self.status.load_state_dict(self.pre_checkpoint, only_model=True)
             logger.info(f"✅ 加载权重成功！")
 
-        # 设置 device
-        self.status.model.to(self.status.device)
-        self.criterion.to(self.status.device)
-        logger.info(f"✅ 当前训练设备：{self.status.device}")
+        # 检查验证数据集
+        self.status.val_loader = self.factory.create_val_dataloader()
+        if self.status.val_loader is None or len(self.status.val_loader) == 0:
+            logger.warning("⚠️ 未添加验证数据集")
+        else:
+            logger.info(f"✅ 初始化验证数据集: {[name for name in self.status.val_loader.keys()]}")
+
+        # ========= 组装结束 ========
 
         # 创建权重输出文件夹
         if self.autosave:
-            if self.checkpoint_dir == "":
-                raise RuntimeError("❌ Engine Build Error: 权重文件夹未配置。")
+            validator.check(self.checkpoint_dir != "", "❌ Engine Build Error: 权重文件夹未配置。")
             file_io.create_dir(self.checkpoint_dir)
             logger.info(f"✅ 权重保存目录：{self.checkpoint_dir}")
         logger.info(f"{'✅' if self.autosave else '⚠️'} 权重自动保存：{self.autosave}")
