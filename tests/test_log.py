5179b9137bb7c7c9534273787f37afd0f575e1c9
aikwen <kwen.zou@outlook.com>
2025-11-29 02:59:50 +0800

    ✅ test new log

--- tests/test_log.py
+++ tests/test_log.py
@@ -2,10 +2,16 @@ from owl.utils import file_io
 
 if __name__ == '__main__':
 
-    f1 = file_io.create_logger("2025-11-19","train")
-    f2 = file_io.create_logger("2025-11-19", "train")
+    f1 = file_io.get_logger("2025-11-19", "train")
+    f2 = file_io.get_logger("2025-11-19", "train")
 
     f1.info("abc1")
     f2.info("abc2")
     f1.warning("abc3")
 
+    val_log1 = file_io.get_logger("2025-11-19", "val", is_format=False)
+    val_log2 = file_io.get_logger("2025-11-19", "val")
+
+    val_log1.info("abc1")
+    val_log2.info("abc2")
+    
